e90f2278c3b22050894bcfa78c2a0149d0a7ace4
Graham Otte <go@grahamotte.com>
2020-06-01 17:49:53 -0700

    Updates

---
+++ precache-manifest.05eac5c0739c767a6b8bb10be8761ad6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d7160b62758a0452b53460a0898a66",
+    "url": "/imgurand/index.html"
+  },
+  {
+    "revision": "24437abd9834a5aca77e",
+    "url": "/imgurand/static/css/main.e9afa030.chunk.css"
+  },
+  {
+    "revision": "7c407d89583efd88f16b",
+    "url": "/imgurand/static/js/2.b95a4419.chunk.js"
+  },
+  {
+    "revision": "bc0495e3914ed396093e7a7990e70b00",
+    "url": "/imgurand/static/js/2.b95a4419.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24437abd9834a5aca77e",
+    "url": "/imgurand/static/js/main.c2da2f4e.chunk.js"
+  },
+  {
+    "revision": "d0a70cd0381c824f05fe",
+    "url": "/imgurand/static/js/runtime-main.3d234edb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/grahamotte/imgurand/precache-manifest.307bdce867fee1556b76e42b715dd454.js"
+  "/imgurand/precache-manifest.05eac5c0739c767a6b8bb10be8761ad6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/grahamotte/imgurand/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/imgurand/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
